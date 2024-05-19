6084dbf9c411faf1f60647d7cce5c97e6bb61e33
Prashant Kumar <38308359+Pk9697@users.noreply.github.com>
2024-05-19 12:40:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	s1=s1.toLowerCase()
+	s2=s2.toLowerCase()
+
+	// let i=0,j=0;
+	let n1=s1.length,n2=s2.length
+	
+	for(let i=0;i<n1;++i){
+
+		for(let j=0;j<n2;++j){
+			if(s1[i+j]!=s2[j]){
+				break;
+			}
+			if(j==n2-1){
+				return i;
+			}
+		}
+		 
+	}
+
+	return -1;
+	
+	
 }
 
 // Please do not change the code below
